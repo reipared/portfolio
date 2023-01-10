e721f8828425b8452f251435d1782b243c9076c1
reipared <reinaldopared@gmail.com>
2023-01-10 17:24:15 -0400

    updated

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const sections = document.querySelectorAll(".section");
+const secBtns = document.querySelectorAll(".controls");
+const secBtn = document.querySelectorAll(".control");
+const allSections = document.querySelectorAll(".main-content");
+
+function PageTransitions() {
+	// Button 25:34
+}
