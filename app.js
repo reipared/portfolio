401f128cc35f58f8a922758c0fb5f2042f5e43c0
reipared <reinaldopared@gmail.com>
2023-01-16 12:41:28 -0400

    Project continuation

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const sections = document.querySelectorAll(".section");
+/* const sections = document.querySelectorAll(".section");
 const secBtns = document.querySelectorAll(".controls");
 const secBtn = document.querySelectorAll(".control");
 const allSections = document.querySelectorAll(".main-content");
@@ -37,4 +37,18 @@ function PageTransitions() {
 	});
 }
 
-PageTransitions();
+PageTransitions(); */
+
+(function () {
+	[...document.querySelectorAll(".control")].forEach((button) => {
+		button.addEventListener("click", function () {
+			document.querySelector(".active-btn").classList.remove("active-btn");
+			this.classList.add("active-btn");
+			document.querySelector(".active").classList.remove("active");
+			document.getElementById(button.dataset.id).classList.add("active");
+		});
+	});
+	document.querySelector(".theme-btn").addEventListener("click", () => {
+		document.body.classList.toggle("light-mode");
+	});
+})();
